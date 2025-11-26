a6f120812c39ee9677beccbcb6ba11ccf863af37
yeunzu <yj060706@kakao.com>
2025-11-26 22:23:54 +0900

    그래프 그리기 파일 코드 리펙터링

--- drwaing_graph.py
+++ drwaing_graph.py
@@ -9,11 +9,8 @@ df = df.loc[1:]
 # # 보정 예스
 # df = pd.read_csv("")
 
-# 일반경로
-#  
-
 # 이상 경로 구하는 함수(그래프 그리기용, 사각형)
-def draw_answer_route(start, end):
+def draw_correct_route_data(start, end):
     x_1, y_1 = start
     x_2, y_2 = end
     if x_1 > x_2:
@@ -26,12 +23,12 @@ def draw_answer_route(start, end):
         y_range = [y_1]*100 + [i for i in range(y_1, y_2)] + [y_2]*100 + [i for i in range(y_2, y_1, -1)]
     return x_range, y_range
 
-x_range, y_range = draw_answer_route((0, 0), (100, -100))
+x_range, y_range = draw_correct_route_data((0, 0), (100, -100))
 
-plt.plot(df['x_pos'], df['y_pos'], label='real_route', color='red')
-plt.plot(x_range, y_range, ls='--', label='answer_route', color='blue')
+plt.plot(df['x_pos'], df['y_pos'], label='actual_route', color='red')
+plt.plot(x_range, y_range, ls='--', label='correct_route', color='blue')
 plt.hlines(0, -30, 120, ls=':', colors='gray') # x축 긋기
-plt.vlines(0, -150, 25, ls=':', colors='gray')
+plt.vlines(0, -150, 25, ls=':', colors='gray') # y축 긋기
 
 plt.grid()
 plt.legend()
